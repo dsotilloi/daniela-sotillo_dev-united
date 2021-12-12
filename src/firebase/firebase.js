2b4098c50c9fd65ff9374e6f0217ebec8695adce
Daniela Sotillo <sotillo.daniela@gmail.com>
2021-12-12 01:31:08 -0300

    Se importaron funcionalidades de auth de Firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import "firebase/auth";
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyB_jRlthKb30eUve6IVUq7nHjGeoD_l9_I",
@@ -14,5 +16,14 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 // Exporta la funcionalidad de la DB
 export const firestore = firebase.firestore();
+//módulo de autenticación:
+export const auth = firebase.auth();
+//el proveedor de la autenticación:
+export const provider = new firebase.auth.GoogleAuthProvider();
+//la utilidad para hacer login con el pop-up:
+export const loginConGoogle = () => auth.signInWithPopup(provider);
+//la utilidad para hacer logout:
+export const logout = () => auth.signOut();
+
 // exporta el paquete de firebase para poder usarlo
 export default firebase;
