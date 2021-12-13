59bebeacee3e10cf70b6dc1a8c3e17f15c0d2f23
Daniela Sotillo <sotillo.daniela@gmail.com>
2021-12-13 11:55:52 -0300

    Se importaron componetes de react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import { AuthenticationContext } from './hooks/context/AuthenticationContext';
 import PainterProvider from './hooks/context/PainterContext';
-import Header from "./components/Header";
-import Main from "./components/Main";
+import UserFeed from "./components/UserFeed"
+import UserRegister from "./components/UserRegister";
 import Footer from "./components/Footer";
 import './styles/app.css';
 
 function App() {
+
+  const { user, logout } = useContext(AuthenticationContext);
+  console.log(user)
+
   return (
     <div className="App">
-      <PainterProvider>
-        <Header />
-        <Main />
-      </PainterProvider>
-      <Footer />
+        <PainterProvider>
+
+      <Routes>
+
+        {user ? (
+          <Route path="/feed" element={ <UserFeed /> } />
+        ):(
+          <Route path="/register" element={ <UserRegister /> } />
+          )
+        }
+      </Routes>
+
+        </PainterProvider>
+
+        <Footer />
+
+        <Link to="/feed">
+          <button>Ir al feed</button>      
+        </Link>
+
+        <button onClick={logout}>Cerrar sesión</button>      
+
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import AuthenticationProvider from './hooks/context/AuthenticationContext';
 import App from './App';
 import './index.css';
@@ -7,7 +8,9 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <AuthenticationProvider>
-      <App />
+      <Router>
+          <App />
+      </Router>
     </AuthenticationProvider>
   </React.StrictMode>,
   document.getElementById('root')
