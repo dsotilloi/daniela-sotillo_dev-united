c447adc0055e3353f7aafb0a8a7c52fa722d3044
Daniela Sotillo <sotillo.daniela@gmail.com>
2021-12-13 11:55:05 -0300

    Se crearon compeonentes para enrutamiento

---
+++ src/components/UserFeed.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function UserFeed() {
+
+  return (
+    <section className="user-feed">
+        <p>USER FEED</p>
+    </section>
+  );
+}
+
+export default UserFeed;
---
+++ src/components/UserRegister.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+
+function UserRegister() {
+
+  return (
+    <section className="user-register">
+        <Header />
+        <Main />
+    </section>
+  );
+}
+
+export default UserRegister;
