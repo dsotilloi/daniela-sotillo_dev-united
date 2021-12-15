4abbccdbeadf94db0c0d0562e7440376a2001d44
Daniela Sotillo <sotillo.daniela@gmail.com>
2021-12-15 15:29:43 -0300

    Se crearon rutas para UserFeed y 404

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AuthenticationProvider from './hooks/context/AuthenticationContext';
 import App from './App';
+import UserFeed from "./components/UserFeed";
+import NotFound404 from './components/NotFound404';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthenticationProvider>
       <Router>
-          <App />
+        <Routes>
+          <Route path="/" element={ <App /> } />
+          <Route path="/feed" element={ <UserFeed /> } />
+          <Route path="*" element={ <NotFound404 /> } />
+        </Routes>
       </Router>
     </AuthenticationProvider>
   </React.StrictMode>,
