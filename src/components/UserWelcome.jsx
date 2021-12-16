c71f8d3e4708b43d203471781e3434e822732e50
Daniela Sotillo <sotillo.daniela@gmail.com>
2021-12-15 21:43:16 -0300

    Se actualizó intercambio de data con Firestore

--- src/components/UserWelcome.jsx
+++ src/components/UserWelcome.jsx
@@ -1,24 +1,76 @@
-import React, { useState, useContext } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { firestore, auth } from "../firebase/firebase";
 import { PainterContext } from '../hooks/context/PainterContext';
 import { cta } from "../helpers/button-cta";
+
 import Button from "./Button";
 import ColorPicker from "./ColorPicker";
+
 import '../styles/userWelcome.css';
 
 const UserWelcome = () => {
 
-	const { colorsList, handleColor, stateColor } = useContext(PainterContext);
-	const [ inputData, setInputData ] = useState("");
+	const { colorsList, stateColor, setStateColor } = useContext(PainterContext);
+	const navigate = useNavigate();
 
+	const [ color, setColor ] = useState({ color: "" })
+	const [ userLogged, setUserLogged ] = useState([]);
+  const [ user, setUser ] = useState(null);
+	const [ username, setUsername ] = useState({ uid: "",	author: "" })
 
-	const handleInputData = (e) => {
-		setInputData(e.target.value);
+	useEffect(() => {
+
+		const cancelSubscription = firestore
+			.collection( "posts" )
+			.onSnapshot(( snapshot ) => {
+				const userInfo = snapshot.docs.map((doc) => {
+					return {
+						id: doc.id,
+						uid: doc.data().username.uid,
+						author: doc.data().username.author,
+						color: doc.data().color.color
+					};
+				});
+
+				setUserLogged(userInfo);
+
+			})
+
+			auth.onAuthStateChanged(( user ) => {
+				setUser(user);
+			});
+
+			return () => cancelSubscription();
+
+	}, [])
+	
+	const handleInput = (e) => {
+		let username = {
+			author: e.target.value,
+      uid: user.uid
+		};
+		setUsername( username );
 	}
 
+	const handleColor = ( colorSelected ) => {
+		let color = {
+			color: colorSelected,
+		};
+		setColor( color );
+		setStateColor( true );
+	}
 
-	// console.log(inputData);
-	// console.log(colorSelected);
+	const handleUserStyle = async (e) => {
+		e.preventDefault();
+    await firestore.collection( "posts" ).add( { username, color } );
+		navigate("feed");
+	}
+
+	///////////////////////////////////////////////////
+
+	console.log(userLogged);
+	console.log(user);
 	// console.log(stateColor);
 	
 	return (
@@ -28,9 +80,9 @@ const UserWelcome = () => {
 			
 			<input 
 				type="text" 
-				onChange={handleInputData} 
+				onChange={ handleInput } 
 				placeholder="Type your username" 
-				value={inputData}
+				value={ username.author }
 				/>
 
 			<p>Select your favorite color:</p>
@@ -39,16 +91,14 @@ const UserWelcome = () => {
 				{colorsList.map((color) => 
 						<ColorPicker 
 						color={ color } 
-						handleColor={ handleColor } 
+						handle={ handleColor } 
 						key={ color.hex } />
 					)
 				}
 			</ul>
 			
-			{inputData !== "" && stateColor && (
-				<Link to="/feed">
-					<Button cta={cta.continue} />
-				</Link>
+			{username.author !== "" && stateColor && (
+					<Button cta={ cta.continue } handle={ handleUserStyle } />
 				)
 			}
 
