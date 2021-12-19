57fd4ed295787eda66c43520cd12ba72043c4904
Daniela Sotillo <sotillo.daniela@gmail.com>
2021-12-19 13:18:02 -0300

    Las rutas se pasaron de index.js a App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { useContext } from "react";
-import PainterProvider from './hooks/context/PainterContext';
-import { AuthenticationContext } from './hooks/context/AuthenticationContext';
-import { Outlet } from "react-router-dom";
-import UserRegister from "./components/UserRegister";
-import Footer from "./components/Footer";
+import { AppContext } from './hooks/context/AppContext';
+import { Routes, Route } from "react-router-dom";
+import UserRegister from "./components/routes/UserRegister";
+import UserFeed from "./components/routes/UserFeed";
+import NotFound404 from './components/routes/NotFound404';
+import Footer from "./components/presentational/Footer";
 import './styles/app.css';
 
 function App() {
 
-  const { logout } = useContext(AuthenticationContext);
+  const { logout } = useContext(AppContext);
   return (
     <div className="App">
-      <PainterProvider>
 
-        <UserRegister />
+        <Routes>
+          <Route path="/" element={ <UserRegister /> } />
+          <Route path="/feed" element={ <UserFeed /> } />
+          <Route path="*" element={ <NotFound404 /> } />
+        </Routes>
 
-      </PainterProvider>
-      
       <Footer />
-
-      <Outlet />
 
       <button onClick={logout}>Cerrar sesión</button>  
     </div>
--- src/index.js
+++ src/index.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AuthenticationProvider from './hooks/context/AuthenticationContext';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppProvider from './hooks/context/AppContext';
 import App from './App';
-import UserFeed from "./components/UserFeed";
-import NotFound404 from './components/NotFound404';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthenticationProvider>
+    <AppProvider>
       <Router>
-        <Routes>
-          <Route path="/" element={ <App /> } />
-          <Route path="/feed" element={ <UserFeed /> } />
-          <Route path="*" element={ <NotFound404 /> } />
-        </Routes>
+        <App />
       </Router>
-    </AuthenticationProvider>
+    </AppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
