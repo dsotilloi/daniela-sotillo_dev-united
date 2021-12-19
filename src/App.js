84ddb45ab073ea69d86ac7cec863ab8e7f863dd5
Daniela Sotillo <sotillo.daniela@gmail.com>
2021-12-19 18:02:41 -0300

    Se creó ruta para <UserProfile/>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useContext } from "react";
 import { AppContext } from './hooks/context/AppContext';
 import { Routes, Route } from "react-router-dom";
-import UserRegister from "./components/routes/UserRegister";
-import UserFeed from "./components/routes/UserFeed";
-import NotFound404 from './components/routes/NotFound404';
+import UserRegister from "./components/presentational/UserRegister";
+import UserFeed from "./components/containers/UserFeed";
+import UserProfile from "./components/containers/UserProfile";
+import NotFound404 from './components/presentational/NotFound404';
 import Footer from "./components/presentational/Footer";
 import './styles/app.css';
 
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={ <UserRegister /> } />
           <Route path="/feed" element={ <UserFeed /> } />
+          <Route path="/profile" element={ <UserProfile /> } />
           <Route path="*" element={ <NotFound404 /> } />
         </Routes>
 
