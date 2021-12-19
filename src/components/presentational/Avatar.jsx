3dcbcd13cf3d9189c64f8fbe522bf6f4c09eef82
Daniela Sotillo <sotillo.daniela@gmail.com>
2021-12-19 18:05:05 -0300

    Se agregaron imágenes y handles

--- src/components/presentational/Avatar.jsx
+++ src/components/presentational/Avatar.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 
-function Avatar({ src }) {
+function Avatar({ src, handle }) {
 	return (
 		<div>
-			<img src={ src } alt="user photo" />
+			<img src={ src } alt="avatar" onClick={ handle } />
 	  </div>
-
 	);
-  }
+}
   
-  export default Avatar;
+export default Avatar;
--- src/components/presentational/PostContent.jsx
+++ src/components/presentational/PostContent.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Avatar from "./Avatar";
 
-function PostContent({ src, message, date }) {
-  // console.log('hola  dadada')
+function PostContent({ src, message, date, nickname, like, counterLikes, handle }) {
+
+  const image = require.context( '../../assets/images', true );
+
     return (
       <div>
         <Avatar src={ src } />
+        <p>{ nickname }</p>
+        <p>date</p>
         <p>{ message }</p>
-        <p>{ date }</p>
-        <p>MESSAGEEEE</p>
+        <p>{ like }</p>
+        <p>{ counterLikes }</p>
+        <img src={ image(`./delete-icon.svg`).default } alt="delete icon" onClick={ handle }/>
       </div>
     );
   }
