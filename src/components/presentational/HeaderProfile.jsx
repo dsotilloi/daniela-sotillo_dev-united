75793cdd513ef62de71888d80b080a320268e4cb
Daniela Sotillo <sotillo.daniela@gmail.com>
2021-12-19 18:02:06 -0300

    Se creó componente HeaderProfile

---
+++ src/components/presentational/HeaderProfile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Button from "./Button";
+import Avatar from "./Avatar";
+
+
+function HeaderProfile({ src, cta, handle, nickname }) {
+
+	const image = require.context( '../../assets/images', true );
+
+  return (
+		<header>
+			<img src={ image(`./back-icon.svg`).default } alt='back icon' />
+			<p>{ nickname }</p>
+			<Button cta={ cta } handle={ handle } />
+			<Avatar src={ src } />
+			<p>{ nickname }</p>
+		</header>
+  );
+}
+  
+export default HeaderProfile;
