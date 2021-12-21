49ae82be023c3b52552007d66cb732a718c6bb92
Daniela Sotillo <sotillo.daniela@gmail.com>
2021-12-20 23:40:39 -0300

    Se pasaron props handles para dar like/unlike

--- src/components/presentational/PostContent.jsx
+++ src/components/presentational/PostContent.jsx
@@ -1,21 +1,38 @@
 import React from "react";
 import Avatar from "./Avatar";
 
-function PostContent({ src, message, date, nickname, like, counterLikes, handle, postId }) {
+function PostContent({ 
+  src, 
+  message, 
+  date, 
+  nickname, 
+  like,
+  counterLikes, 
+  handleDelete, 
+  postId,
+  handleLike,
+  handleUnlike
+}) {
 
   const image = require.context( '../../assets/images', true );
 
-    return (
-      <div>
-        <Avatar src={ src } />
-        <p>{ nickname }</p>
-        <p>date</p>
-        <p>{ message }</p>
-        <p>{ like }</p>
-        <p>{ counterLikes }</p>
-        <img src={ image(`./delete-icon.svg`).default } alt="delete icon" onClick={ () => handle( postId ) }/>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Avatar src={ src } />
+      <p>{ nickname }</p>
+      <p>date</p>
+      <img src={ image(`./delete-icon.svg`).default } alt="delete icon" onClick={ () => handleDelete( postId )} />
+      <p>{ message }</p>
+
+      {like ? (
+        <img src={ image(`./like-on.svg`).default } alt="like icon" onClick={() => handleUnlike( postId, like )} />
+        ):( 
+        <img src={ image(`./like-off.svg`).default } alt="dislike icon" onClick={() => handleLike( postId, like )} />
+      )}
+
+      <p>{ counterLikes ? counterLikes : 0 }</p>
+    </div>
+  );
+}
   
-  export default PostContent;
+export default PostContent;
