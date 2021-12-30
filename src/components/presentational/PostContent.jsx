db769abc460a4d37fd501ff261752db52ac8a66a
Daniela Sotillo <sotillo.daniela@gmail.com>
2021-12-30 16:29:48 -0300

    Se crearon funciones para filtrar la lista de posts

--- src/components/presentational/PostContent.jsx
+++ src/components/presentational/PostContent.jsx
@@ -1,36 +1,53 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AppContext } from "../../hooks/context/AppContext";
+
 import Avatar from "./Avatar";
 
+
 function PostContent({ 
-  src, 
-  message, 
-  date, 
-  nickname, 
-  like,
-  counterLikes, 
   handleDelete, 
-  postId,
   handleLike,
-  handleUnlike
+  handleUnlike,
+  message, 
+  nickname, 
+  photo,
+  postId
 }) {
 
+  const { postsList, user } = useContext(AppContext);
   const image = require.context( '../../assets/images', true );
+
+  //Filtra la lista de post según el postId al cual el usuario de click: 
+  const filterPosts = postsList.filter(( post ) => post.id === postId );
+
+  //Setea el timestamp del post:
+  const postTimestamp = filterPosts.map((post) => post.date.seconds * 1000);
+  
+  //Encuentra el uid del usuario en el array de likes en Firebase:
+  const userLikes = filterPosts.map(( fav ) => fav.likes.find(( uidFav ) => user.uid === uidFav));
+
+  //Setea la información del post para que pueda ser renderizada en el componente:
+  const post = {
+    like: userLikes.includes( user.uid ),
+    countLikes: filterPosts.map((post) => post.likes.length),
+    localeDate: new Date(...postTimestamp).toLocaleDateString()
+  };
 
   return (
     <div>
-      <Avatar src={ src } />
+      <Avatar src={ photo } />
       <p>{ nickname }</p>
-      <p>date</p>
+      <p>{ post.localeDate }</p>
       <img src={ image(`./delete-icon.svg`).default } alt="delete icon" onClick={ () => handleDelete( postId )} />
       <p>{ message }</p>
 
-      {like ? (
-        <img src={ image(`./like-on.svg`).default } alt="like icon" onClick={() => handleUnlike( postId, like )} />
+      {post.like ? (
+        <img src={ image(`./like-on.svg`).default } alt="like icon" onClick={() => handleUnlike( postId )} />
         ):( 
-        <img src={ image(`./like-off.svg`).default } alt="dislike icon" onClick={() => handleLike( postId, like )} />
+        <img src={ image(`./like-off.svg`).default } alt="dislike icon" onClick={() => handleLike( postId )} />
       )}
 
-      <p>{ counterLikes ? counterLikes : 0 }</p>
+      <p>{ post.countLikes }</p>
     </div>
   );
 }
