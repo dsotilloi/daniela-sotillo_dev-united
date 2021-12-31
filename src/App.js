ea99d16fa0f3fd85f58c5c973f112821a50e6df1
Daniela Sotillo <sotillo.daniela@gmail.com>
2021-12-31 13:31:29 -0300

    Se cambiaron las rutas de Feed y Home

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useContext } from "react";
 import { AppContext } from './hooks/context/AppContext';
 import { Routes, Route } from "react-router-dom";
 import UserRegister from "./components/presentational/UserRegister";
-import Feed from "./components/containers/Feed";
+import Home from "./components/containers/Home";
+import Feed from "./components/presentational/Feed";
 import Profile from "./components/containers/Profile";
 import NotFound404 from './components/presentational/NotFound404';
 import Footer from "./components/presentational/Footer";
@@ -16,8 +17,10 @@ function App() {
 
         <Routes>
           <Route path="/" element={ <UserRegister /> } />
-          <Route path="/feed" element={ <Feed /> } />
-          <Route path="/profile" element={ <Profile /> } />
+          <Route path="/home" element={ <Home /> }>
+            <Route index element={ <Feed /> } />
+            <Route path="profile" element={ <Profile /> } />
+          </Route>
           <Route path="*" element={ <NotFound404 /> } />
         </Routes>
 
