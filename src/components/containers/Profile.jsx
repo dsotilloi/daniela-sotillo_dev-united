fc0287994327aaf85e3bb6528285675f2764c6fd
Daniela Sotillo <sotillo.daniela@gmail.com>
2021-12-31 13:36:12 -0300

    Se crearon los filtros de post en <Profile/>

--- src/components/containers/Profile.jsx
+++ src/components/containers/Profile.jsx
@@ -1,57 +1,79 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "../../hooks/context/AppContext";
 import { cta } from "../../helpers/button-cta";
 import { sortPostsList } from "../../helpers/sortPostsList";
-
 
 import HeaderProfile from "../presentational/HeaderProfile";
 import Button from "../presentational/Button";
 import PostContent from "../presentational/PostContent";
 
 
-function Profile() {
+function Profile({ deletePost, likePost, unlikePost }) {
 
-  // const image = require.context( '../../assets/images', true );
-  const { user, logout, postsList } = useContext( AppContext );
+  const { user, logout, postsList, loggedUsers } = useContext( AppContext );
+  const [ showUserPosts, setUserShowPosts ] = useState( true );
+  
+  let filteredList = [];
+    
+  //Filtra la lista de posts hechos por el usuario, si "showUserPosts" es true. 
+  //Si es false, el filtro devuelve los post favoritos del usuario:
+  if( showUserPosts ) {
+    filteredList = postsList.filter(( post ) => post.authorUid === user.uid);
+  }else{
+    filteredList = postsList.filter((post) => post.likes.find(( uidLike ) => user.uid === uidLike));
+  };
 
-  const showUserPosts = () => {
-    const filterPosts = postsList.filter(( post ) => post.authorUid === user.uid);
-    console.log(filterPosts);
-  }
+  //Manejan el estado "showUserPosts":
+  const viewUserPosts = () => {
+    setUserShowPosts(true);
+  };
+
+  const viewFavPosts = () => {
+    setUserShowPosts(false);
+  };
+
+  const infoUser = loggedUsers.filter(( logged ) => logged.uid === user.uid).map(( logged ) => {
+    return {
+      nickname: logged.nickname,
+      photo: logged.photo
+    }
+  });
 
   //Ordena lista de post según la fecha de publicación:
   sortPostsList( postsList );
 
   return (
     <>
-      {user && 
+      {infoUser.map(( info ) => 
         <HeaderProfile 
-        cta={ cta.logout } 
-        src={ user.photoURL } 
-        // nickname={  } 
-        handle={ logout } />
-      }
+          cta={ cta.logout } 
+          handle={ logout } 
+          key={ info.nickname }
+          nickname={ info.nickname } 
+          src={ info.photo } 
+        />
+      )}
 
       <main>
-
         <section>
-          <Button cta={cta.posts} handle={ showUserPosts } />
-          <Button cta={cta.favorites}/>
+          <Button cta={cta.posts} handle={ viewUserPosts } />
+          <Button cta={cta.favorites} handle={ viewFavPosts }/>
         </section>
 
-        {postsList.map((post)=> (
+          {filteredList.map((post)=> (
             <PostContent 
               authorUid={ post.authorUid }
+              deletePost={ deletePost }
               key={ post.id } 
+              likePost={ likePost }
               message={ post.message } 
               nickname={ post.authorNickname }
               photo={ post.authorPhoto}
               postId={ post.id }
+              unlikePost={ unlikePost }
             />
           ))}
-
       </main>
-
     </>
   );
 }
