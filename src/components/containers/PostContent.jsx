323659603562593b234c5a7c2686b2bef8307caf
Daniela Sotillo <sotillo.daniela@gmail.com>
2022-01-02 08:10:43 -0300

    Componente PostContent fue cambiado de directorio a "Containers"

--- src/components/containers/PostContent.jsx
+++ src/components/containers/PostContent.jsx
@@ -1,23 +1,25 @@
 import React, { useContext } from "react";
+import { Link } from "react-router-dom";
 import { AppContext } from "../../hooks/context/AppContext";
+import firebase, { firestore } from "../../firebase/firebase";
 
-import Avatar from "./Avatar";
+
+import Avatar from "../presentational/Avatar";
 
 
 function PostContent({ 
   authorUid,
-  deletePost, 
-  likePost,
   message, 
   nickname, 
   photo,
-  postId,
-  unlikePost
+  postId
 }) {
 
-  // const [ showDelete, setShowDelete ] = useState( test2 );
   const { postsList, user } = useContext(AppContext);
   const image = require.context( '../../assets/images', true );
+
+  //Setea el enlace del avatar del post:
+  const url = authorUid === user.uid ? '/profile' : `/profile/${ authorUid }`;
 
   //Filtra la lista de posts según el postId al cual el usuario de click: 
   const filteredList = postsList.filter(( post ) => post.id === postId );
@@ -30,14 +32,38 @@ function PostContent({
 
   //Setea la información del post para que pueda ser renderizada en el componente:
   const post = {
-    countLikes: filteredList.map((post) => post.likes.length),
+    countLikes: filteredList.map(( post ) => post.likes.length),
     like: userLikes.includes( user.uid ),
-    localeDate: new Date(...postTimestamp).toLocaleDateString()
+    localeDate: new Date( ...postTimestamp ).toLocaleDateString()
   };
+
+  //Elimina el post de Firebase:
+	const deletePost = ( postId ) => {
+		if(window.confirm( '¡Hey! ¿Seguro quieres borrar tu post? 😱' )){
+			firestore.doc( `posts/${ postId }` ).delete();
+			alert( '¡Sigue adelante! Inspírate y comparte otro ahora 💪💡')
+		}
+	};
+
+	//Actualiza el array de likes agregando el uid del usuario da like:
+	const likePost = ( postId ) => {
+		firestore.doc( `posts/${ postId }` ).update({
+			likes: firebase.firestore.FieldValue.arrayUnion( user.uid )
+		});
+	};
+
+	//Actualiza el array de likes eliminando el uid del usuario que quita el like:
+	const unlikePost = ( postId ) => {
+		firestore.doc( `posts/${ postId }` ).update({
+			likes: firebase.firestore.FieldValue.arrayRemove( user.uid )
+		});
+	}; 
 
   return (
     <div>
-      <Avatar src={ photo } />
+      <Link to={ url }>
+        <Avatar src={ photo } />
+      </Link>
       <p>{ nickname }</p>
       <p>{ post.localeDate }</p>
 
