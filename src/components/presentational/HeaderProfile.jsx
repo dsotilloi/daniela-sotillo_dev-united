795ce805d14a58a59674754a215c2ae4438b0cd6
Daniela Sotillo <sotillo.daniela@gmail.com>
2022-01-04 16:04:05 -0300

    Se cambió handle para "ir atrás" en la navegación

--- src/components/presentational/HeaderProfile.jsx
+++ src/components/presentational/HeaderProfile.jsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React from 'react';
+import { logout } from "../../firebase/firebase";
+import { useNavigate } from 'react-router-dom';
 
-import Button from "./Button";
-import Avatar from "./Avatar";
+import Button from './Button';
+import Avatar from './Avatar';
 
 
-function HeaderProfile({ src, cta, handle, nickname }) {
+function HeaderProfile({ src, cta, nickname }) {
 
 	const image = require.context( '../../assets/images', true );
+	const navigate = useNavigate();
+	
+	const goBack =() => {
+		navigate(-1);
+	};
+
+	const userLogout = () => {
+		logout();
+		navigate('/');
+	};
 
   return (
 		<header>
-			<img src={ image(`./back.svg`).default } alt='back icon' />
+			<img 
+				src={ image(`./back.svg`).default } 
+				alt='back icon'
+				onClick={ goBack } 
+			/>
+
 			<p>{ nickname }</p>
-			<Button cta={ cta } handle={ handle } />
+			<Button cta={ cta } handle={ userLogout } />
 			<Avatar src={ src } />
 			<p>{ nickname }</p>
 		</header>
