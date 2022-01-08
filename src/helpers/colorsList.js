76b0dffca432b9cfbdb8056231da90c066890faa
Daniela Sotillo <sotillo.daniela@gmail.com>
2022-01-08 19:20:19 -0300

    Se incorporó en la lista de colores la propiedad "selected"

--- src/helpers/colorsList.js
+++ src/helpers/colorsList.js
@@ -1,8 +1,8 @@
 export const colorsList = [
-    { name: "red", hex: "#F50D5A" },
-    { name: "orange", hex: "#FF865C" },
-    { name: "yellow", hex: "#FFEA5C" },
-    { name: "green", hex: "#00DA76" },
-    { name: "blue", hex: "#0096CE" },
-    { name: "violet", hex: "#800FFF" }
-  ];
+  { name: "red", hex: "#F50D5A", selected: false },
+  { name: "orange", hex: "#FF865C", selected: false },
+  { name: "yellow", hex: "#FFEA5C", selected: false },
+  { name: "green", hex: "#00DA76", selected: false },
+  { name: "blue", hex: "#0096CE", selected: false },
+  { name: "violet", hex: "#800FFF", selected: false }
+];
